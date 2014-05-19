6f498f61576ade62012cb64ca7968c1b00229b0f
Filip Ginter <ginter@cs.utu.fi>
2014-05-19 12:23:20 +0300

    Adding an older hfst-ol.jar to support pre-java1.7 systems before the official one on the HFST tools page is updated

--- install.sh
+++ install.sh
@@ -53,6 +53,38 @@ function select_hunpos_binary {
     echo "kindly contribute the compiled binary to the project: ginter@cs.utu.fi   Thank you!"
     return 1
 }
+
+function select_hfstol {
+    echo "Selecting the hfst-ol.jar to run:"
+    #Do we already have a working one in in LIBS?
+    T=$(echo "koiransa" | java -jar hfst-ol.jar ../model/morphology.finntreebank.hfstol 2>&1 | grep -c 'Ready for input')
+    if (( "$T" == "1" ))
+    then
+	echo "...seems to have been done already, works."
+	return 0
+    fi
+
+    #Try the distributed binaries until you find one which works
+    for ol in ../LIBS-LOCAL/hfst-ol/hfst-ol.jar.*
+    do
+	echo "Trying $ol"
+	T=$(echo "koiransa" | java -jar $ol ../model/morphology.finntreebank.hfstol 2>&1 | grep -c 'Ready for input')
+	if (( "$T" == "1" ))
+	then
+	#it works! symlink it
+	    echo "Works. Selecting it"
+	    ln -f -s $ol ./hfst-ol.jar
+	    return 0
+	fi
+    done
+    echo
+    echo "Couldn't get any of the hfst-ol.jar versions in LIBS-LOCAL/hfst-ol to work"
+    echo
+    echo "Try to compile it yourself from the sources on the HFST project pages"
+    echo "If you can produce a working .jar file, copy or symlink it as LIBS/hfst-ol.jar and"
+    echo "kindly contribute the compiled binary to the project: ginter@cs.utu.fi   Thank you!"
+    return 1
+}
     
 
 function echoc {
@@ -80,9 +112,9 @@ runc wget http://mirror.netinch.com/pub/apache/opennlp/opennlp-1.5.3/apache-open
 runc tar zxf apache-opennlp-1.5.3-bin.tar.gz
 
 runc select_hunpos_binary  #This checks which hunpos works (the downloaded binary, or the binary included here) and makes a symlink to LIBS/hunpos-tag
+runc select_hfstol
 
 runc cd ..
-echo "SUCCESS"
 echo
 echo "Now try"
 echo "cat data/wiki-test.txt | ./parser_wrapper.sh > wiki-test-parsed.txt"
