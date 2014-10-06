e23a0e09c277d72cad7d986995674c19e1ab30e5
Filip Ginter <ginter@cs.utu.fi>
2014-10-06 18:20:19 +0300

    linking roots

--- limit_sentence_len.py
+++ limit_sentence_len.py
@@ -5,8 +5,7 @@ import codecs
 
 out8=codecs.getwriter("utf-8")(sys.stdout)
 
-ID=0
-FORM=1
+ID,FORM,LEMMA,PLEMMA,POS,PPOS,FEAT,PFEAT,HEAD,PHEAD,DEPREL,PDEPREL=range(12)
 
 def cutCandidates(sent):
     #Cut-point candidates for sent (list of CoNLL09 columns)
@@ -64,7 +63,30 @@ def print_sent(sent,comments,add_newline=True):
     if add_newline:
         print >> out8
 
-def renumber
+def renumber(sent,offset):
+    #Renumber all tokens and head references by a given offset (can be negative)
+    for cols in sent:
+        for col in (ID,HEAD,PHEAD): #renumber these columns, unless they are a "_"
+            if cols[col]!=u"_":
+                cols[col]=unicode(int(cols[col])+offset)
+
+def get_root(sent):
+    """
+    On the off-chance there'd be several roots, let's dep them together and return the first one
+    """
+    r=[]
+    for idx,cols in enumerate(sent):
+        if cols[HEAD]==u"0":
+            r.append(idx)
+    if len(r)>1: #Ooops - this really happens?
+        for i in range(1,len(r)):
+            gov,d_row=r[i-1][ID],r[i] #gov is the string with ID
+            d_row[HEAD]=gov
+            d_row[PHEAD]=gov
+            d_row[DEPREL]=u"dep"
+            d_row[PDEPREL]=u"dep"
+    return int(sent[r[0]][ID])-1 #the row index of the root
+        
 
 def cutAndPrintSentence(sent,comments):
     subsents=cutSent(sent)
@@ -86,16 +108,24 @@ if __name__=="__main__":
     args = parser.parse_args()
     args.leeway=args.chunk_size//3 #TODO - better value maybe?
 
-    lastlen=None
     if args.reverse:
+        last_len=None
+        last_root=None
         for sent,comments in read_conll(args.input,0):
-            if len(comments)==1 and comments[-1]==u"#---sentence---splitter---JOIN-TO-PREVIOUS-SENTENCE---":
-                renumber(sent,lastlen)
+            if len(comments)==1 and comments[0]==u"#---sentence---splitter---JOIN-TO-PREVIOUS-SENTENCE---":
+                part_root=get_root(sent) #root of this chunk
+                renumber(sent,last_len)
+                sent[part_root][HEAD]=unicode(last_root+1) #...dep to the previous one
+                sent[part_root][PHEAD]=unicode(last_root+1)
+                sent[part_root][DEPREL]=u"dep"
+                sent[part_root][PDEPREL]=u"dep"
+                last_root=int(sent[part_root][ID])-1 #...and remember the renumbered root for the possible next one
                 print_sent(sent,[],False)
-                lastlen+=len(sent)
+                last_len+=len(sent)
             else:
-                if lastlen is not None:
+                if last_len is not None:
                     print >> out8
+                last_root=get_root(sent) 
                 print_sent(sent,comments,False)
                 last_len=len(sent)
         else:
