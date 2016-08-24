45d00a3cf92955b1bd2c075555a2d5bf1f850088
Filip Ginter <ginter@cs.utu.fi>
2016-08-24 12:49:16 +0300

    Adding a 5-second timeout on lookup, after which the lookup is restarted and empty analysis given

--- omorfi_wrapper.py
+++ omorfi_wrapper.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import logging
+import select
 logging.basicConfig(level=logging.WARNING)
 
 import subprocess
@@ -19,10 +20,20 @@ class OmorfiWrapper(object):
             raise Exception("No transducer file found: %s" % transducer_file)
 
         self.log = logging.getLogger("hfst")
+        self.process=None
+        self.transducer_file=transducer_file
+        self.poll=None
+        self.restart()
 
+
+    def restart(self):
+        if self.process is not None:
+            #Try to kill
+            self.process.terminate()
+        self.poll=select.poll()
         try:
             self.log.info("Starting hfst-ol.jar")
-            self.process = subprocess.Popen(["java","-jar", os.path.join(SCRIPTDIR,"LIBS","hfst-ol.jar"), transducer_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            self.process = subprocess.Popen(["java","-jar", os.path.join(SCRIPTDIR,"LIBS","hfst-ol.jar"), self.transducer_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             if self.process.returncode!=None: #Ended already - something's wrong
                 self.log.debug("Non-zero exit code for hfst-ol.jar")
                 raise HFSTError()
@@ -37,6 +48,8 @@ class OmorfiWrapper(object):
             self.log.error("Did not succeed in launching 'java -jar LIBS/hfst-ol.jar %s'. The most common reason for this is that you forgot to run './install.sh'. Run it, and also run 'test_dependencies.py' to make sure all is OK.\n\nIf it fails even though you did succeed with ./install.sh, try to run 'java -jar LIBS/hfst-ol.jar model/morphology.finntreebank.hfstol'. It should start and ask for input with 'Ready for input.' Then type in 'koiransa' and see if you get a reasonable analysis. Then either open an issue at https://github.com/TurkuNLP/Finnish-dep-parser/issues  or email ginter@cs.utu.fi and jmnybl@utu.fi and we'll try to help you.\n\nGiving up, because the parser cannot run without morphological lookup."%transducer_file)
             sys.exit(1)
         self.log.info("Started the HFST process.")
+        self.poll.register(self.process.stdout)
+        
 
 
     def lookup(self, word):
@@ -44,6 +57,9 @@ class OmorfiWrapper(object):
         self.process.stdin.write(word+"\n")
         self.process.stdin.flush()
         results = []
+        if not self.poll.poll(5000): #Nothing ready in five seconds - we're stuck
+            self.restart()
+            return []
         for line in iter(self.process.stdout.readline,''):
             if line.strip() == "":
                 break
